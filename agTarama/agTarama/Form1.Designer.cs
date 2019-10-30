849b24f07f6ff568e365bf38efe77c12dae48eac
volkan cengiz <volkancengiz@outlook.com>
2019-10-30 10:09:54 +0300

    Add files via upload

---
+++ agTarama/agTarama/Form1.Designer.cs
@@ -0,0 +1,84 @@
+﻿namespace agTarama
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.listBox2 = new System.Windows.Forms.ListBox();
+            this.listBox3 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // listBox1
+            // 
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.Location = new System.Drawing.Point(12, 12);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(102, 108);
+            this.listBox1.TabIndex = 0;
+            // 
+            // listBox2
+            // 
+            this.listBox2.FormattingEnabled = true;
+            this.listBox2.Location = new System.Drawing.Point(120, 12);
+            this.listBox2.Name = "listBox2";
+            this.listBox2.Size = new System.Drawing.Size(121, 225);
+            this.listBox2.TabIndex = 1;
+            // 
+            // listBox3
+            // 
+            this.listBox3.FormattingEnabled = true;
+            this.listBox3.Location = new System.Drawing.Point(247, 12);
+            this.listBox3.MultiColumn = true;
+            this.listBox3.Name = "listBox3";
+            this.listBox3.ScrollAlwaysVisible = true;
+            this.listBox3.Size = new System.Drawing.Size(486, 251);
+            this.listBox3.TabIndex = 2;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(745, 273);
+            this.Controls.Add(this.listBox3);
+            this.Controls.Add(this.listBox2);
+            this.Controls.Add(this.listBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.ListBox listBox2;
+        private System.Windows.Forms.ListBox listBox3;
+    }
+}
+
---
+++ agTarama/agTarama/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Net;
+using System.Text;
+using System.Windows.Forms;
+using System.Net.NetworkInformation;
+using System.IO;
+
+namespace agTarama
+{
+    public partial class Form1 : Form
+    {
+        String[] ipAdresleri;
+        public Form1()
+        {
+            InitializeComponent();
+            
+            bilgisayarlariBul();
+            
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        public void DirSearch(string dir)
+        {
+            listBox2.Items.Add(dir);
+            try
+            {
+                foreach (string f in Directory.GetFiles(dir))
+                    listBox3.Items.Add(f);
+                foreach (string d in Directory.GetDirectories(dir))
+                {
+                    listBox3.Items.Add(d);
+                    DirSearch(d);
+                }
+
+            }
+            catch (System.Exception ex)
+            {
+                listBox3.Items.Add(ex.ToString());
+            }
+        }
+
+        public void tara (String subnet,Int16 sayac)
+        {
+            Ping paket;
+            PingReply cevap;
+            IPAddress adresler;
+            IPHostEntry host;
+
+            for(int i=1;i<255;i++)
+            {
+                String sonSayi = "." + i.ToString();
+                paket = new Ping();
+                string data = " ";
+                byte[] buffer = Encoding.ASCII.GetBytes(data);
+                cevap = paket.Send(subnet + sonSayi,100,buffer);
+                if(cevap.Status==IPStatus.Success)
+                {
+                    try
+                    {
+                        adresler = IPAddress.Parse(subnet + sonSayi);
+                        host = Dns.GetHostEntry(adresler);
+                        String yol = subnet + sonSayi ;
+                        DirSearch(@"\\LENOVO");
+                        listBox1.Items.Add(yol);
+                        ipAdresleri[sayac] = subnet + sonSayi;
+
+
+                    }
+                    catch
+                    {
+
+                    }
+                }
+            }
+        }
+
+        public void bilgisayarlariBul()
+        {
+            IPHostEntry host;
+            Int16 sayac = 0;
+            string yerelIP = " ? ";
+            host = Dns.GetHostEntry(Dns.GetHostName());
+
+            foreach (IPAddress ipAd in host.AddressList)
+            {
+                if (ipAd.AddressFamily.ToString() == "InterNetwork")
+                {
+                    yerelIP = ipAd.ToString();
+                    //listBox1.Items.Add(yerelIP);
+                    string[] adresler = yerelIP.Split('.');
+                    yerelIP = adresler[0] + "." + adresler[1] + "." + adresler[2];
+                    tara(yerelIP,sayac);
+                    sayac++;
+                }
+            }
+        }
+
+
+    }
+}
